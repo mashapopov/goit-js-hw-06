8fd40c1769ac9ce116b78b1de41dc08b85fcf38d
Maria Popova <0979867251m@gmail.com>
2023-01-17 00:17:25 +0200

    8dz

--- js/task-03.js
+++ js/task-03.js
@@ -12,22 +12,6 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
-// const navImg = document.querySelector('ul.gallery')
-// const imageEl = document.createElement('img');
-// const listEl = document.createElement('li');
-// imageEl.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-// imageEl.alt = 'White and Black Long Fur Cat';
-// imageEl.width = 200;
-// listEl.appendChild(imageEl);
-// navImg.appendChild(listEl);
-// console.log(navImg);
-// console.log(listEl);
-
-
-
-
-
-
 const list = document.querySelector("ul.gallery");
 
 const galleryPictures = images.map(
@@ -41,18 +25,3 @@ list.insertAdjacentHTML("beforeend", galleryPictures);
 
 
 
-// <style>
-//       img {
-//         display: block;
-//         max-width: 100%;
-//         height: auto;
-//       }
-//       ul {
-//         margin: 0;
-//         padding: 0;
-//       }
-//       .gallery {
-//         list-style: none;
-//         display: block;
-//       }
-//     </style>
--- js/task-08.js
+++ js/task-08.js
@@ -1,33 +1,33 @@
 
-// const form = document.querySelector("form.login-form");
-// const emailInput = document.querySelector(`input[name="email"]`);
-// const passwordInput = document.querySelector(`input[name="password"]`);
 
-// form.addEventListener("input", handleInput);
-// form.addEventListener("submit", handleSubmit);
+const form = document.querySelector("form.login-form");
+const emailInput = document.querySelector(`input[name="email"]`);
+const passwordInput = document.querySelector(`input[name="password"]`);
 
-// const users = [];
-// let user = {};
+form.addEventListener("input", handleInput);
+form.addEventListener("submit", handleSubmit);
 
-// function handleInput(event) {
-//   event.preventDefault();
-//   user[event.target.name] = event.target.value;
-// }
+const users = [];
+let user = {};
 
-// function handleSubmit(event) {
-//   event.preventDefault();
+function handleInput(event) {
+  event.preventDefault();
+  user[event.target.name] = event.target.value;
+}
 
-//   if (emailInput.value === "" || passwordInput.value === "") {
-//     return alert("Все поля должны быть заполнены");
-//   }
-//   // if (event.target.value == "") {
-//   // }
-//   users.push(user);
+function handleSubmit(event) {
+  event.preventDefault();
 
-//   user = {};
-//   event.target.reset();
-//   users.map((user) =>
-//     console.log(`Email: ${user.email}
-// Password: ${user.password}`)
-//   );
-// }
+  if (emailInput.value === "" || passwordInput.value === "") {
+    return alert("Все поля должны быть заполнены");
+  }
+ 
+  users.push(user);
+
+  user = {};
+  event.target.reset();
+  users.map((user) =>
+    console.log(`Email: ${user.email}
+Password: ${user.password}`)
+  );
+}
