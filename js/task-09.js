f21b5c10e9abedaeabe6fbe9760b9f64446293bb
Maria Popova <0979867251m@gmail.com>
2023-01-17 00:37:17 +0200

    z9

--- js/task-09.js
+++ js/task-09.js
@@ -1,22 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+const body = document.querySelector("body");
+const buttonChangeColor = document.querySelector("button.change-color");
+const spanColor = document.querySelector("span.color");
+buttonChangeColor.addEventListener("click", onHandleClick);
 
-
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-
-// const body = document.querySelector("body");
-// const buttonChangeColor = document.querySelector("button.change-color");
-// const spanColor = document.querySelector("span.color");
-
-// buttonChangeColor.addEventListener("click", onHandleClick);
-
-// function onHandleClick(event) {
-//   let changeBackgroundColor = getRandomHexColor();
-//   spanColor.textContent = `${changeBackgroundColor}`;
-//   body.style.backgroundColor = `${changeBackgroundColor}`;
-//   console.log((body.style.backgroundColor = `${changeBackgroundColor}`));
-// }
+function onHandleClick(event) {
+  let changeBackgroundColor = getRandomHexColor();
+  spanColor.textContent = `${changeBackgroundColor}`;
+  body.style.backgroundColor = `${changeBackgroundColor}`;
+  console.log((body.style.backgroundColor = `${changeBackgroundColor}`));
+}
